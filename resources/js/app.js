ce83e2c8f0ab58c0d78e6d0ae50a2a4b188e9b92
Ilya Savich <ilia.savich.97@gmail.com>
2018-11-07 15:57:22 +0300

    one room chat

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,8 +15,47 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('chat-message', require('./components/ChatMessage.vue'));
+Vue.component('chat-log', require('./components/ChatLog.vue'));
+Vue.component('chat-composer', require('./components/ChatComposer.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        messages: [],
+        usersInRoom: []
+    },
+    methods: {
+        addMessage(message) {
+            // Add to existing messages
+            this.messages.push(message);
+
+            // Persist to the database etc
+            axios.post('/messages', message).then(response => {
+                // Do whatever;
+            })
+        }
+    },
+    created() {
+        axios.get('/messages').then(response => {
+            this.messages = response.data;
+        });
+
+        Echo.join('chatroom')
+            .here((users) => {
+                this.usersInRoom = users;
+            })
+            .joining((user) => {
+                this.usersInRoom.push(user);
+            })
+            .leaving((user) => {
+                this.usersInRoom = this.usersInRoom.filter(u => u != user);
+            })
+            .listen('MessagePosted', (e) => {
+                this.messages.push({
+                    message: e.message.message,
+                    user: e.user
+                });
+            });
+    }
 });
